7a6b86d409a518fc55cf4697bbc43f463672125a
Serge78 <nandeserge91@gmail.com>
2026-01-22 12:45:25 +0100

    fix: correction import middleware dans attendance routes

--- routes/attendance.routes.js
+++ routes/attendance.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authenticate, authorize } from '../middleware/auth.js';
+import { authenticate, authorize } from '../middlewares/auth.middleware.js';
 import { body, validationResult } from 'express-validator';
 import prisma from '../lib/prisma.js';
 
